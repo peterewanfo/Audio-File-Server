93e4793f7493e29bf4378fd6bd88c1945d71f7f6
peterewanfo <56024478+peterewanfo@users.noreply.github.com>
2021-02-25 09:00:15 +0100

    Add files via upload

---
+++ Backend/app/business_logic/utils/Decorators.py
@@ -0,0 +1,18 @@
+import jwt, time
+from functools import wraps
+from config import config
+from flask import request, make_response
+from app.business_logic.utils.DBFunctionsClass import DBFunctionsClass
+
+def manage_db_connection(func):
+    @wraps(func)
+    def wrapper_function(*args, **kwargs):
+
+        function_return_data = func(*args, **kwargs)
+
+        #MANAGE DATABASE CONNECTION
+        DBFunctionsClass.manageDBConnection()
+
+        return function_return_data
+
+    return wrapper_function
